47d9399e12844237d1ae4019b3317dcc6f76497d
Sreekuttan M Achari <sreekuttan.mohanan@pearldatadirect.com>
2022-05-31 11:58:22 +0530

    lib added

--- api.js
+++ api.js
@@ -1,3 +1,8 @@
+const eps = require('./lib/entrypoints');
+const idm = require('./lib/idm');
+
 exports.printMsg = function() {
     console.log("This is a message from the demo package");
+    eps.printMsg();
+    idm.printMsg();
   }
