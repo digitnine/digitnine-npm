67fff79d9aa824b1b9547cb6eca2a64da8fb5adf
syam s menon <syam.menon@pearldatadirect.com>
2022-06-03 11:04:57 +0530

    gentbenficary id api added

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 if (typeof localStorage === "undefined" || localStorage === null) {
-  var LocalStorage = require('node-localstorage').LocalStorage;
-  localStorage = new LocalStorage('./node_modules/@digitnine/digitnine/tmp');
+    var LocalStorage = require('node-localstorage').LocalStorage;
+    localStorage = new LocalStorage('./node_modules/@digitnine/digitnine/tmp');
 }
 
 const eps = require('./lib/entrypoints');
@@ -11,66 +11,78 @@ const VERSION = '1.0.0';
 
 module.exports = {
 
-  appsDetails: { 'ver': VERSION },
-  key: '',
-  secret: '',
-  token: '',
-  cacheToken: false,
-  isProduction: false,
+    appsDetails: { 'ver': VERSION },
+    key: '',
+    secret: '',
+    token: '',
+    cacheToken: false,
+    isProduction: false,
 
-  printMsg: function () {
-    console.log("This is a message from the Digit9 API package");
-    eps.printMsg();
-    idm.printMsg();
-    merchant.printMsg();
+    printMsg: function () {
+        console.log("This is a message from the Digit9 API package");
+        eps.printMsg();
+        idm.printMsg();
+        merchant.printMsg();
 
-    console.log(eps.isProduction);
-    console.log(eps.getIDMServiceUrl());
-    console.log(eps.getMerchantServiceUrl());
+        console.log(eps.isProduction);
+        console.log(eps.getIDMServiceUrl());
+        console.log(eps.getMerchantServiceUrl());
 
-    eps.isProduction = this.isProduction;
-    console.log(eps.isProduction);
-    console.log(eps.getIDMServiceUrl());
-    console.log(eps.getMerchantServiceUrl());
-  },
+        eps.isProduction = this.isProduction;
+        console.log(eps.isProduction);
+        console.log(eps.getIDMServiceUrl());
+        console.log(eps.getMerchantServiceUrl());
+    },
 
-  getToken: async function () {
-    data = { 'username': this.key, 'password': this.secret }
-    this.token = localStorage.getItem('token');
+    getToken: async function () {
+        data = { 'username': this.key, 'password': this.secret }
+        this.token = localStorage.getItem('token');
 
-    if (this.token != '' && this.token != undefined) {
-      console.log("Catched: ", this.token);
-      return this.token;
-    } else {
-      this.token = await idm.getToken(data)
-      console.log("Fetched: ", this.token);
-      if (this.cacheToken) {
-        localStorage.setItem('token', this.token);
-      }
-      return this.token;
+        if (this.token != '' && this.token != undefined) {
+            console.log("Catched: ", this.token);
+            return this.token;
+        } else {
+            this.token = await idm.getToken(data)
+            console.log("Fetched: ", this.token);
+            if (this.cacheToken) {
+                localStorage.setItem('token', this.token);
+            }
+            return this.token;
 
-      ;
-    }
-  },
+            ;
+        }
+    },
 
-  getCurrentUserDetails: async function () {
-    await this.getToken();
-    if (this.token !== null) {
-      idm.authToken = this.token;
-      return idm.getCurrentUserDetails()
-    } else {
-      throw new Error('No token');
-    }
-  },
+    getCurrentUserDetails: async function () {
+        await this.getToken();
+        if (this.token !== null) {
+            idm.authToken = this.token;
+            return idm.getCurrentUserDetails()
+        } else {
+            throw new Error('No token');
+        }
+    },
 
-  getWallets: async function (pagination_data = null) {
-    await this.getToken();
-    if (this.token !== null) {
-      merchant.authToken = this.token;
-      return merchant.getWallets();
-    } else {
-      throw new Error('No token');
-    }
-  },
+    getWallets: async function (pagination_data = null) {
+        await this.getToken();
+        if (this.token !== null) {
+            merchant.authToken = this.token;
+            
+            return merchant.getWallets();
+        } else {
+            throw new Error('No token');
+        }
+    },
+
+    getBeneficiaryId: async function (id) {
+        await this.getToken();
+        if (this.token !== null) {
+            merchant.authToken = this.token;
+           
+            return merchant.getBeneficiaryId(id)
+        } else {
+            throw new Error('No token');
+        }
+    },
 
 };
--- test.js
+++ test.js
@@ -1,19 +1,25 @@
 const api = require('./api');
+const merchant = require('./lib/merchant');
 
 api.key = "sp2";
 api.secret = "password";
 api.isProduction = false;
 
-api.cacheToken = true ;
+api.cacheToken = true;
 
 //api.printMsg();
 
-api.getCurrentUserDetails().then(user => {
-    console.log(user);
-    return user;
-});
+// api.getCurrentUserDetails().then(user => {
+//     console.log(user);
+//     return user;
+// });
 
 // api.getWallets().then(wallets => {
 //     console.log(wallets);
 //     return wallets;
 // });
+
+api.getBeneficiaryId({'id':7}).then(response => {
+    //console.log(response);
+    return response;
+})
