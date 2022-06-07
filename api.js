3c9cf9450767148dc8d307cbbeec5ec892caba2b
elvin.lloyd <elvin.lloyd@pearldatadirect.com>
2022-06-07 15:41:12 +0530

    apis added

--- api.js
+++ api.js
@@ -89,7 +89,7 @@ module.exports = {
         }
     },
 
-    //ervice Provider Merchant Profile operations Apis--
+    //Service Provider Merchant Profile operations Apis--
     getMerchantProfileById: async function (id) {
         await this.getToken();
         if (this.token !== null) {
@@ -100,4 +100,165 @@ module.exports = {
             throw new Error('No token');
         }
     },
+    //List Merchant profile under current service provider--
+    listMerchantProfiles: async function ({page = 0,size=20,merchantName,sort=[]}) {
+        await this.getToken();
+        if (this.token !== null) {
+            merchant.authToken = this.token;
+
+            return merchant.listMerchantProfiles({page,size,merchantName,sort});
+        } else {
+            throw new Error('No token');
+        }
+    },
+    //Update Merchant Profile operations Apis--
+    updateMerchantProfileById: async function (id ,body) {
+        if(id!==null && id!==undefined){
+            if(body!==null && body!==undefined){
+                await this.getToken();
+                if (this.token !== null) {
+                    merchant.authToken = this.token;
+
+                    return merchant.updateMerchantProfileById(id,body)
+                } else {
+                    throw new Error('No token');
+                }
+            }
+            else{
+                throw new Error('body is required');
+            }
+        }
+        else{
+            throw new Error('id is required');
+        }
+    },
+
+    //List service provider--
+    listServiceProviders: async function ({countryCode, currencyCode, page=0, size=20, sort=[]}) {
+        if(countryCode!==null && countryCode!==undefined && countryCode!==""){
+            if(currencyCode!==null && currencyCode!==undefined && currencyCode!==""){
+                await this.getToken();
+                if (this.token !== null) {
+                    merchant.authToken = this.token;
+
+                    return merchant.listServiceProviders({countryCode,currencyCode,page,size,sort});
+                } else {
+                    throw new Error('No token');
+                }
+            }
+            else{
+                throw new Error('currencyCode is required');
+            }
+        }else{
+            throw new Error('countryCode is required');
+        }
+    },
+
+    //List of banks associated to a service provider id--
+    listBanksOfServiceProvider: async function ({serviceProviderId, countryCode, currencyCode, page=0, size=20, sort=[]}) {
+        if(serviceProviderId!==null && serviceProviderId!==undefined && serviceProviderId!==""){
+            if(countryCode!==null && countryCode!==undefined && countryCode!==""){
+                if(currencyCode!==null && currencyCode!==undefined && currencyCode!==""){
+                    await this.getToken();
+                    if (this.token !== null) {
+                        merchant.authToken = this.token;
+
+                        return merchant.listBanksOfServiceProvider({serviceProviderId,countryCode,currencyCode,page,size,sort});
+                    } else {
+                        throw new Error('No token');
+                    }
+                }
+                else{
+                    throw new Error('currencyCode is required');
+                }
+            }else{
+                throw new Error('countryCode is required');
+            }
+        }
+        else{
+            throw new Error('serviceProviderId is required');
+        }
+    },
+
+    //get cureent rate dto--
+    getCurrentRate: async function ({fromCurrency, toCurrency, amount=1}) {
+        if(fromCurrency!==null && fromCurrency!==undefined && fromCurrency!==""){
+            if(toCurrency!==null && toCurrency!==undefined && toCurrency!==""){
+                await this.getToken();
+                if (this.token !== null) {
+                    merchant.authToken = this.token;
+
+                    return merchant.getCurrentRate({fromCurrency,toCurrency,amount});
+                } else {
+                    throw new Error('No token');
+                }
+            }
+            else{
+                throw new Error('toCurrency is required');
+            }
+        }else{
+            throw new Error('fromCurrency is required');
+        }
+    },
+
+    //list executive documents dto--
+    listExecutiveDocuments: async function () {
+        await this.getToken();
+        if (this.token !== null) {
+            merchant.authToken = this.token;
+
+            return merchant.listExecutiveDocuments();
+        } else {
+            throw new Error('No token');
+        } 
+    },
+
+    //list fees dto of service provider--
+    listFees: async function () {
+        await this.getToken();
+        if (this.token !== null) {
+            merchant.authToken = this.token;
+
+            return merchant.listFees();
+        } else {
+            throw new Error('No token');
+        } 
+    },
+
+    //list KYC dto of service provider--
+    listServiceProviderKYC: async function () {
+        await this.getToken();
+        if (this.token !== null) {
+            merchant.authToken = this.token;
+
+            return merchant.listServiceProviderKYC();
+        } else {
+            throw new Error('No token');
+        } 
+    },
+
+    //list merchant wallets dto--
+    listMerchantWallets: async function ({merchantName,page,size,sort=[]}) {
+        await this.getToken();
+        if (this.token !== null) {
+            merchant.authToken = this.token;
+
+            return merchant.listMerchantWallets({merchantName,page,size,sort});
+        } else {
+            throw new Error('No token');
+        } 
+    },
+
+    //list service provider risk scoring assesments--
+    listRiskScoringAssesments: async function () {
+        await this.getToken();
+        if (this.token !== null) {
+            merchant.authToken = this.token;
+
+            return merchant.listRiskScoringAssesments();
+        } else {
+            throw new Error('No token');
+        } 
+    },
+
 };
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-const api = require('./api');
-const merchant = require('./lib/merchant');
+const api = require("./api");
+const merchant = require("./lib/merchant");
 
 api.key = "sp2";
 api.secret = "password";
@@ -14,12 +14,97 @@ api.cacheToken = true;
 //     return user;
 // });
 
-api.getWallets().then(wallets => {
-    //console.log(wallets);
-    return wallets;
-});
+// api.getWallets().then(wallets => {
+//     //console.log(wallets);
+//     return wallets;
+// });
 
-api.getMerchantProfileById('7').then(response => {
-    //console.log(response);
-    return response;
-})
+// api.getMerchantProfileById('7').then(response => {
+//     //console.log(response);
+//     return response;
+// })
+// api.listMerchantProfiles({
+//         page: 0,
+//         size: 10,
+//         merchantName: "name",
+//         sort: ["sort1" , "sort2"]
+//     })
+//     .then((response) => {
+//         //console.log(wallets);
+//         return response;
+//     });
+
+// api.updateMerchantProfileById("7",{"body": "test"}).then((response) => {
+//   //console.log(response);
+//   return response;
+// });
+
+// api
+//   .listServiceProviders({
+//     countryCode: "sdcsdf",
+//     currencyCode: "sda",
+//     // page: 0,
+//     // size: 10,
+//     sort: ["sort1", "sort2"],
+//   })
+//   .then((response) => {
+//     //console.log(wallets);
+//     return response;
+//   });
+
+// api
+//   .listBanksOfServiceProvider({
+//     serviceProviderId: "aS",
+//     countryCode: "sdcsdf",
+//     currencyCode: "sda",
+//     // page: 0,
+//     // size: 10,
+//     sort: ["sort1", "sort2","SJNFJNS"],
+//   })
+//   .then((response) => {
+//     //console.log(wallets);
+//     return response;
+//   });
+
+// api
+//   .getCurrentRate({
+//     fromCurrency: "aS",
+//     toCurrency: "sdcsdf",
+//     amount: 10,
+//   })
+//   .then((response) => {
+//     //console.log(wallets);
+//     return response;
+//   });
+
+// api.listExecutiveDocuments().then((response) => {
+//   //console.log(wallets);
+//   return response;
+// });
+
+// api.listFees().then((response) => {
+//   //console.log(wallets);
+//   return response;
+// });
+
+// api.listServiceProviderKYC().then((response) => {
+//   //console.log(wallets);
+//   return response;
+// });
+
+// api
+//   .listMerchantWallets({
+//     // merchantName: "sdcsdf",
+//     page: 0,
+//     size: 10,
+//     sort: ["sort1", "sort2"],
+//   })
+//   .then((response) => {
+//     //console.log(wallets);
+//     return response;
+//   });
+
+api.listRiskScoringAssesments().then((response) => {
+  //console.log(wallets);
+  return response;
+});
